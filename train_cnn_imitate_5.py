a3dbb42b2d7234b7b44690700d75f51a0947b4cd
TKHuang <tkhuangs@gmail.com>
2020-04-22 02:13:44 +0800

    fix wrong use in syntax and format all code

--- train_cnn_imitate_5.py
+++ train_cnn_imitate_5.py
@@ -20,19 +20,25 @@ def toonehot(text):
 
 # Create CNN Model
 print("Creating CNN model...")
-in = Input((60, 200, 3))
-out = in
-out = Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu')(out)
+input = Input((60, 200, 3))
+out = input
+out = Conv2D(
+    filters=32, kernel_size=(3, 3), padding='same', activation='relu')(
+        out)
 out = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(out)
 out = BatchNormalization()(out)
 out = MaxPooling2D(pool_size=(2, 2))(out)
 out = Dropout(0.3)(out)
-out = Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(out)
+out = Conv2D(
+    filters=64, kernel_size=(3, 3), padding='same', activation='relu')(
+        out)
 out = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(out)
 out = BatchNormalization()(out)
 out = MaxPooling2D(pool_size=(2, 2))(out)
 out = Dropout(0.3)(out)
-out = Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(out)
+out = Conv2D(
+    filters=128, kernel_size=(3, 3), padding='same', activation='relu')(
+        out)
 out = Conv2D(filters=128, kernel_size=(3, 3), activation='relu')(out)
 out = BatchNormalization()(out)
 out = MaxPooling2D(pool_size=(2, 2))(out)
@@ -47,14 +53,20 @@ out = [Dense(34, name='digit1', activation='softmax')(out),\
     Dense(34, name='digit3', activation='softmax')(out),\
     Dense(34, name='digit4', activation='softmax')(out),\
     Dense(34, name='digit5', activation='softmax')(out)]
-model = Model(inputs=in, outputs=out)
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model = Model(inputs=input, outputs=out)
+model.compile(
+    loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
 print("Reading training data...")
-traincsv = open('./data/5_imitate_train_set/captcha_train.csv', 'r', encoding = 'utf8')
-train_data = np.stack([np.array(Image.open("./data/5_imitate_train_set/" + row[0] + ".jpg"))/255.0 for row in csv.reader(traincsv)])
-traincsv = open('./data/5_imitate_train_set/captcha_train.csv', 'r', encoding = 'utf8')
+traincsv = open(
+    './data/5_imitate_train_set/captcha_train.csv', 'r', encoding='utf8')
+train_data = np.stack([
+    np.array(Image.open("./data/5_imitate_train_set/" + row[0] + ".jpg")) /
+    255.0 for row in csv.reader(traincsv)
+])
+traincsv = open(
+    './data/5_imitate_train_set/captcha_train.csv', 'r', encoding='utf8')
 read_label = [toonehot(row[1]) for row in csv.reader(traincsv)]
 train_label = [[] for _ in range(5)]
 for arr in read_label:
@@ -64,9 +76,14 @@ train_label = [arr for arr in np.asarray(train_label)]
 print("Shape of train data:", train_data.shape)
 
 print("Reading validation data...")
-valicsv = open('./data/5_imitate_vali_set/captcha_vali.csv', 'r', encoding = 'utf8')
-vali_data = np.stack([np.array(Image.open("./data/5_imitate_vali_set/" + row[0] + ".jpg"))/255.0 for row in csv.reader(valicsv)])
-valicsv = open('./data/5_imitate_vali_set/captcha_vali.csv', 'r', encoding = 'utf8')
+valicsv = open(
+    './data/5_imitate_vali_set/captcha_vali.csv', 'r', encoding='utf8')
+vali_data = np.stack([
+    np.array(Image.open("./data/5_imitate_vali_set/" + row[0] + ".jpg")) / 255.0
+    for row in csv.reader(valicsv)
+])
+valicsv = open(
+    './data/5_imitate_vali_set/captcha_vali.csv', 'r', encoding='utf8')
 read_label = [toonehot(row[1]) for row in csv.reader(valicsv)]
 vali_label = [[] for _ in range(5)]
 for arr in read_label:
@@ -75,9 +92,22 @@ for arr in read_label:
 vali_label = [arr for arr in np.asarray(vali_label)]
 print("Shape of validation data:", vali_data.shape)
 
-filepath="./data/model/imitate_5_model.h5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_digit5_acc', verbose=1, save_best_only=True, mode='max')
-earlystop = EarlyStopping(monitor='val_digit5_acc', patience=5, verbose=1, mode='auto')
-tensorBoard = TensorBoard(log_dir = "./logs", histogram_freq = 1)
+filepath = "./data/model/imitate_5_model.h5"
+checkpoint = ModelCheckpoint(
+    filepath,
+    monitor='val_digit5_acc',
+    verbose=1,
+    save_best_only=True,
+    mode='max')
+earlystop = EarlyStopping(
+    monitor='val_digit5_acc', patience=5, verbose=1, mode='auto')
+tensorBoard = TensorBoard(log_dir="./logs", histogram_freq=1)
 callbacks_list = [checkpoint, earlystop, tensorBoard]
-model.fit(train_data, train_label, batch_size=400, epochs=100, verbose=2, validation_data=(vali_data, vali_label), callbacks=callbacks_list)
+model.fit(
+    train_data,
+    train_label,
+    batch_size=400,
+    epochs=100,
+    verbose=2,
+    validation_data=(vali_data, vali_label),
+    callbacks=callbacks_list)
